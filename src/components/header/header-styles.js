786029e8c1972021a77ca7d7ff6811e334dcc0d8
Serhii Vedmediev <vss679kn@gmail.com>
2022-08-24 12:41:00 +0300

    headers, src/images, run error in index-styles.js or index.js

--- src/components/header/header-styles.js
+++ src/components/header/header-styles.js
@@ -1,8 +1,7 @@
 import { Link } from 'gatsby'
 import styled from 'styled-components';
-import { DiScala } from 'react-icons/di'
 
-export const HeaderWrapper = styled.div`
+export const HeaderWrapper = styled.header`
     display: flex;
     flex-direction: row;
     justify-content: space-between;
@@ -19,5 +18,59 @@ export const HeaderLogo = styled(Link)`
     align-items: center;
 `
 
-export const HeaderMenu = styled.nav`
+export const HeaderNavMenu = styled.nav`
+font-size: 18px;
+`
+
+export const HeaderNavList = styled.ul`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    list-style: none;
+    text-align: center;
+
+    @media screen and (max-width: 960px) {
+        display: flex;
+        flex-direction: column;
+        width: 100%;
+        /* height: 90vh; */
+        /* position: absolute; */
+        /* top: ${({ click }) => click ? "100%" : "-1000px"}; */
+        opacity: 1;
+        transition: all 0.2s ease;
+    }
+`
+
+export const NavItem = styled.li`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    padding-left: 25px;
+    /* height: 80px; */
+
+    /* @media screen and (max-width: 960px) {
+        width: 100%;
+    } */
+`
+
+export const NavLink = styled(Link)`
+    color: black;
+    display: flex;
+    align-items: center;
+    text-decoration: none;
+`
+
+
+export const LanguageSpan = styled.span`
+    cursor: pointer;
+    text-decoration: none;
+    outline-style: none; 
+    -webkit-touch-callout: none;
+    -webkit-tap-highlight-color: rgba(0,0,0,0);
+
+    &.active {
+        color: #02319B;
+        font-size: 26px;
+    }
+  
 `
--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,8 +1,11 @@
 import React, { useState, useEffect } from 'react'
-import { HeaderWrapper, HeaderIcon, HeaderLogo, HeaderMenu } from './header-styles'
+import { HeaderWrapper, HeaderIcon, HeaderLogo, HeaderNavMenu, NavItem, NavLink, HeaderNavList, LanguageSpan } from './header-styles'
 
 const Header = () => {
 
+    const languages = ['en', 'ua']
+
+    const [language, setLanguage] = useState(languages[0]);
 
     return (
         <>
@@ -10,9 +13,28 @@ const Header = () => {
                 <HeaderLogo to="/">
                     LOGO
                 </HeaderLogo>
-                <HeaderMenu>
-
-                </HeaderMenu>
+                <HeaderNavMenu>
+                    <HeaderNavList>
+                        <NavItem >
+                            <NavLink to="/about">About Us</NavLink>
+                        </NavItem>
+                        <NavItem >
+                            <NavLink to="/about-flutter">About Flutter</NavLink>
+                        </NavItem>
+                        <NavItem >
+                            <NavLink to="/services">Services</NavLink>
+                        </NavItem>
+                        <NavItem >
+                            <NavLink to="/our-work">Our Work</NavLink>
+                        </NavItem>
+                        <NavItem >
+                            <NavLink to="/our-work">Contact Us</NavLink>
+                        </NavItem>
+                        <NavItem >
+                            <LanguageSpan className={`${language === languages[0] ? 'active' : ''} `} onClick={() => setLanguage(languages[0])}>en&nbsp;</LanguageSpan>/&nbsp;<LanguageSpan className={`${language === languages[1] ? 'active' : ''}`} onClick={() => setLanguage(languages[1])}>ua</LanguageSpan>
+                        </NavItem>
+                    </HeaderNavList>
+                </HeaderNavMenu>
             </HeaderWrapper>
         </>
     )
