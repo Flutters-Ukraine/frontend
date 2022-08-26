776eb06a1e4ceecee8530227262ecd9e4e83ac28
Serhii Vedmediev <vss679kn@gmail.com>
2022-08-26 14:45:48 +0300

    about flutter component

--- src/components/about-flutter-component/about-flutter-styles.js
+++ src/components/about-flutter-component/about-flutter-styles.js
@@ -2,13 +2,35 @@ import { Link } from 'gatsby'
 import styled from 'styled-components';
 
 export const AboutFlutterWrapper = styled.div`
-
+    margin-top: 150px;
 `
 
 export const AboutFlutterGrid = styled.div`
-
+    padding-top: 150px;
+    display:grid;
+    grid-template-columns: 1fr 1fr;
+    grid-row: auto auto;
+    grid-column-gap: 20px;
+    grid-row-gap: 200px;
 `
 
 export const AboutFlutterGridItem = styled.div`
 
+`
+
+export const AboutFlutterGridItemTitle = styled.h4`
+	font-size: 50px;
+	font-weight: 500;
+
+	@media screen and (max-width: 1024px) {
+       font-size: 30px;
+    }
+
+	@media screen and (max-width: 960px) {
+       font-size: 20px;
+    }
+`
+
+export const AboutFlutterGridItemText = styled.p`
+	padding-top: 20px;
 `
--- src/components/about-flutter-component/index.js
+++ src/components/about-flutter-component/index.js
@@ -1,7 +1,7 @@
 
 import React, { useState, useEffect } from 'react'
 import DescriptionTextComponent from '../description-text'
-import { AboutFlutterWrapper, AboutFlutterGrid, AboutFlutterGridItem } from '../about-flutter-component/about-flutter-styles'
+import { AboutFlutterWrapper, AboutFlutterGrid, AboutFlutterGridItem, AboutFlutterGridItemTitle, AboutFlutterGridItemText } from '../about-flutter-component/about-flutter-styles'
 
 const AboutFlutterComponent = () => {
     return (
@@ -9,10 +9,22 @@ const AboutFlutterComponent = () => {
             <AboutFlutterWrapper>
                 <DescriptionTextComponent text={'Flutter transforms the app development process. Build, test, and deploy beautiful mobile, web, desktop, and embedded apps from a single codebase.'} />
                 <AboutFlutterGrid>
-                    <AboutFlutterGridItem></AboutFlutterGridItem>
-                    <AboutFlutterGridItem></AboutFlutterGridItem>
-                    <AboutFlutterGridItem></AboutFlutterGridItem>
-                    <AboutFlutterGridItem></AboutFlutterGridItem>
+                    <AboutFlutterGridItem>
+                        <AboutFlutterGridItemTitle>{'Loved by developers 💙'}</AboutFlutterGridItemTitle>
+                        <AboutFlutterGridItemText>{'Overall, 92% of respondents were somewhat satisfied or very satisfied with Flutter.'}</AboutFlutterGridItemText>
+                    </AboutFlutterGridItem>
+                    <AboutFlutterGridItem>
+                        <AboutFlutterGridItemTitle>{'Flutter is fast 🔥'}</AboutFlutterGridItemTitle>
+                        <AboutFlutterGridItemText>{'Flutter code compiles to ARM or Intel machine code as well as JavaScript, for fast performance on any device.'}</AboutFlutterGridItemText>
+                    </AboutFlutterGridItem>
+                    <AboutFlutterGridItem>
+                        <AboutFlutterGridItemTitle>{'Dart language 🐦'}</AboutFlutterGridItemTitle>
+                        <AboutFlutterGridItemText>{'Flutter is powered by Dart, a language optimized for fast apps on any platform.'}</AboutFlutterGridItemText>
+                    </AboutFlutterGridItem>
+                    <AboutFlutterGridItem>
+                        <AboutFlutterGridItemTitle>{'Wide application 📱'}</AboutFlutterGridItemTitle>
+                        <AboutFlutterGridItemText>{'Good both for small MVP\'s and huge scalable apps.'}</AboutFlutterGridItemText>
+                    </AboutFlutterGridItem>
                 </AboutFlutterGrid>
             </AboutFlutterWrapper>
         </>
